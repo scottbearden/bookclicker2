9e8aadaa37da71e228a7e10afbe102024a18fb06
Scott Bearden <18202628+scottbearden@users.noreply.github.com>
2025-07-26 12:48:02 -0400

    Ruby 3.4.5 upgrade

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '3.3.1'
+ruby '3.4.5'
 
 gem 'attr_encrypted'
 gem 'aweber'
--- config/initializers/deny_cloudfront.rb
+++ config/initializers/deny_cloudfront.rb
@@ -1,1 +1,3 @@
+require_relative '../../app/services/cloudfront_denier'
+
 Rails.application.config.middleware.use CloudfrontDenier, target: SITE_URL
