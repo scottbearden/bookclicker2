e861ad43a228cb7cee7b5e4a704d9d471d8a5255
Scott Bearden <18202628+scottbearden@users.noreply.github.com>
2025-07-26 13:33:01 -0400

    Rails 7.2 upgrade

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 ActionMailer::Base.delivery_method = :mailgun
 ActionMailer::Base.mailgun_settings = {
   api_key: ENV['mailgun_api_key'],
-  domain:  ENV['mailgun_domain']
+  domain: ENV['mailgun_domain']
 }
 
 # Initialize the Rails application.
